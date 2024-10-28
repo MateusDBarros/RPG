e99412daad683f901b3164ba3d2bf6d288294ff8
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-10-28 01:53:41 -0300

    Update main.c

--- RPG/main.c
+++ RPG/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct Nave
 {
@@ -10,11 +11,7 @@ struct Nave
     int energia;
 };
 
-void ataque();
-void status();
-int main(void)
-{
-    struct Nave naves[4] = {
+struct Nave naves[4] = {
         {"Astro", 18, 15, 120},
         {"Apollo", 25, 10, 100},
         {"Sombra", 13, 15, 150},
@@ -22,13 +19,78 @@ int main(void)
         };
 
 
+void ataque();
+void status();
+int main(void)
+{
+    srand(time(0));
+    int escolha;
+    struct Nave jogador;
+    struct Nave computador;
+    
+    printf("| %-10s | %-5s | %-5s | %-5s\n", "Nome", "Ataque", "Defesa", "Energia");
+    printf("----------------------------------------\n");
+    for (int i = 0; i < 4; i++) {
+        printf("| %-10s | %-6d | %-6d | %-5d\n",naves[i].nome, naves[i].ataque, naves[i].defesa, naves[i].energia);
+        
+    }
+    do {
+        printf("Escolha sua nave: \n");
+        printf("1. Astro\n2. Apollo\n3. Sombra\n4. Umbra\n(Aperte 5 para sair)\n");
+        scanf("%d", escolha);
+
+        switch (escolha)
+        {
+        case 1:
+            jogador = naves[0];
+            int index = rand() % 4;
+            while (index == escolha) {
+                index = rand() % 4;
+            }
+            computador = naves[index];
+            break;
+        case 2:
+            jogador = naves[1];
+            int index = rand() % 4;
+            while (index == escolha) {
+                index = rand() % 4;
+            }
+            computador = naves[index];
+            break;
+        case 3:
+            jogador = naves[2];
+            int index = rand() % 4;
+            while (index == escolha) {
+                index = rand() % 4;
+            }
+            computador = naves[index];
+            break;
+        case 4:
+            jogador = naves[3];
+            int index = rand() % 4;
+            while (index == escolha) {
+                index = rand() % 4;
+            }
+            computador = naves[index];
+            break;
+        case 5:
+            printf("Encerrando programa...\n");
+            return 0;
+            break;
+        
+        default:
+            printf("opcao invalida!\n");
+            break;
+        }
+    } while(escolha != 5);
+
 }
 
-void ataque(struct Nave nave1, struct Nave nave2) {
-    int dano = nave1.ataque - nave2.defesa;
-    nave2.energia -= dano;
-    printf("%s ataca %s causando %d de dano.\n", nave1.nome, nave2.nome, dano);
-    status(nave1, nave2);
+void ataque(struct Nave *nave1, struct Nave *nave2) {
+    int dano = nave1->ataque - nave2->defesa;
+    nave2->energia -= dano;
+    printf("%s ataca %s causando %d de dano.\n", nave1->nome, nave2->nome, dano);
+    status(*nave1, *nave2);
 }
 
 void status(struct Nave nave1, struct Nave nave2) {
