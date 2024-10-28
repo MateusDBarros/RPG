cdbf61faa1c95724793ff3a3c4d0672a4b6073c6
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-10-28 01:27:11 -0300

    Update main.c

--- RPG/main.c
+++ RPG/main.c
@@ -1,67 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <time.h>
 
-struct Personagem
+struct Nave
 {
-    int hp;
+    char nome[25];
     int ataque;
     int defesa;
-    int agilidade;
+    int energia;
 };
 
-// Protagonista
-struct Personagem protagonista = {100, 20, 10, 15};
-
-// Inimigos
-struct Personagem inimigos[3] = {
-    {80, 15, 5, 10}, {70, 10, 10, 20}, {90, 25, 8, 12}
-};
-
-void batalha(struct Personagem player, struct Personagem inimigo);
+void ataque();
+void status();
 int main(void)
 {
-    srand(time(0));
+    struct Nave naves[4] = {
+        {"Astro", 18, 15, 120},
+        {"Apollo", 25, 10, 100},
+        {"Sombra", 13, 15, 150},
+        {"Umbra", 19, 15, 105}
+        };
 
-    int random = rand() % 3;
-    struct Personagem inimigo = inimigos[random];
 
 }
 
-void batalha(struct Personagem player, struct Personagem inimigo) 
-{
-    int escolha;
-    do
-    {
-        printf("HP:\t %-5d\n", player.hp);
-        printf("HP Inimigo:\t %-5d\n", inimigo.hp);
-        if (player.agilidade > inimigo.agilidade) {
-            printf("1. Atacar.\n");
-            printf("2. Defender.\n");
-            printf("3. Itens.\n");
-            scanf("%d", &escolha);
-            switch (escolha)
-            {
-            case 1:
-                inimigo.hp -= player.ataque;
-                break;
-            case 2:
-                player.hp -= (inimigo.ataque - player.defesa);
-                break;
-            case 3:
-                // Mostra os itens e da opçoes de usar ou de sair 
-                break;
-            
-            default:
-                printf("Escolha invalida!\n");
-                break;
-            }
-        }
-        else {
-            player.hp -= inimigo.ataque;
-        }
+void ataque(struct Nave nave1, struct Nave nave2) {
+    int dano = nave1.ataque - nave2.defesa;
+    nave2.energia -= dano;
+    printf("%s ataca %s causando %d de dano.\n", nave1.nome, nave2.nome, dano);
+    status(nave1, nave2);
+}
 
-        
-    } while (player.hp <= 0 && inimigo.hp <= 0);
-    
+void status(struct Nave nave1, struct Nave nave2) {
+    printf("Energia '%s': %d\n",nave1.nome, nave1.energia);
+    printf("Energia '%s': %d\n",nave2.nome, nave2.energia);
 }
